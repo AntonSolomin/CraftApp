ac2b1caee71d3c42593d1ee38948a2b3bbcc7af9
Anton Solomin <ledorub3000@gmail.com>
2017-09-15 15:32:16 +0300

    Added ApiUtils/Controller

--- src/main/java/com/youCraft/youCraft/PostService.java
+++ src/main/java/com/youCraft/youCraft/PostService.java
@@ -1,5 +1,9 @@
 package com.youCraft.youCraft;
 
+import java.util.List;
+
 public interface PostService {
     Post save(Post post);
+    Post findOne(long id);
+    List<Post> findAll();
 }
--- src/main/java/com/youCraft/youCraft/PostServiceImplementation.java
+++ src/main/java/com/youCraft/youCraft/PostServiceImplementation.java
@@ -3,6 +3,8 @@ package com.youCraft.youCraft;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PostServiceImplementation implements PostService {
     @Autowired
@@ -11,5 +13,15 @@ public class PostServiceImplementation implements PostService {
     @Override
     public Post save(Post post) {
         return postRepository.save(post);
+    }
+
+    @Override
+    public Post findOne(long id) {
+        return postRepository.findOne(id);
+    }
+
+    @Override
+    public List<Post> findAll() {
+        return postRepository.findAll();
     }
 }
